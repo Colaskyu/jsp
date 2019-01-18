fad268a2700d9cfcff46d7eedb46962e9387184f
bigdata <bigdata@808-main>
2019-01-18 11:32:18 +0900

    19/01/18 글삭제 완료

--- board1/WebContent/view.jsp
+++ board1/WebContent/view.jsp
@@ -7,44 +7,8 @@
 <%@page import="kr.co.board1.service.BoardService"%>
 <%@ page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8"%>
 <%
-	request.setCharacterEncoding("UTF-8");
-	String seq = request.getParameter("seq");
-	
-	Connection conn = DBConfig.getConnection();
-	
-	// 3단계
-	PreparedStatement psmt1 = conn.prepareStatement(SQL.SELECT_VIEW);
-	psmt1.setString(1, seq);
-	
-	PreparedStatement psmt2 = conn.prepareStatement(SQL.UPDATE_HIT);
-	psmt2.setString(1, seq);
-	
-	// 4단계	
-	ResultSet rs = psmt1.executeQuery();
-	psmt2.executeUpdate();
-	
-	// 5단계
-	BoardVO vo = new BoardVO();
-	
-	if(rs.next()){
-		vo.setSeq(rs.getInt(1));
-		vo.setParent(rs.getInt(2));
-		vo.setComment(rs.getInt(3));
-		vo.setCate(rs.getString(4));
-		vo.setTitle(rs.getString(5));
-		vo.setContent(rs.getString(6));
-		vo.setFile(rs.getInt(7));
-		vo.setHit(rs.getInt(8));
-		vo.setUid(rs.getString(9));
-		vo.setRegip(rs.getString(10));
-		vo.setRdate(rs.getString(11));
-	}
-	
-	// 6단계
-	rs.close();
-	psmt1.close();
-	psmt2.close();
-	conn.close();
+	BoardService service = BoardService.getInstance();
+	BoardVO vo = service.view(request);
 %>
 
 <!DOCTYPE html>
@@ -84,7 +48,7 @@
 						</tr>
 					</table>
 					<div class="btns">
-						<a href="#" class="cancel del">삭제</a>
+						<a href="./proc/delete.jsp?seq=<%= vo.getSeq() %>" class="cancel del">삭제</a>
 						<a href="#" class="cancel mod">수정</a>
 						<a href="./list.jsp" class="cancel">목록</a>
 					</div>
--- board1/src/kr/co/board1/service/BoardService.java
+++ board1/src/kr/co/board1/service/BoardService.java
@@ -1,5 +1,15 @@
 package kr.co.board1.service;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.http.HttpServletRequest;
+
+import kr.co.board1.config.DBConfig;
+import kr.co.board1.config.SQL;
+import kr.co.board1.vo.BoardVO;
+
 public class BoardService {
 
 	private static BoardService service = new BoardService();
@@ -12,9 +22,68 @@ public class BoardService {
 	
 	public void insertBoard() throws Exception {}
 	public void list() throws Exception {}
-	public void view() throws Exception {}
+	
+	public void updateHit(String seq) throws Exception {
+		
+		Connection conn = DBConfig.getConnection();
+		
+		PreparedStatement psmt = conn.prepareStatement(SQL.UPDATE_HIT);
+		psmt.setString(1, seq);
+		
+		psmt.executeUpdate();
+		psmt.close();
+	}
+	
+	public BoardVO view(HttpServletRequest request) throws Exception {
+		request.setCharacterEncoding("UTF-8");
+		String seq = request.getParameter("seq");
+		
+		// Á¶È¸¼ö +1
+		updateHit(seq);
+		
+		Connection conn = DBConfig.getConnection();
+		
+		PreparedStatement psmt = conn.prepareStatement(SQL.SELECT_VIEW);
+		psmt.setString(1, seq);
+		
+		ResultSet rs = psmt.executeQuery();
+		
+		BoardVO vo = new BoardVO();
+		
+		if(rs.next()){
+			vo.setSeq(rs.getInt(1));
+			vo.setParent(rs.getInt(2));
+			vo.setComment(rs.getInt(3));
+			vo.setCate(rs.getString(4));
+			vo.setTitle(rs.getString(5));
+			vo.setContent(rs.getString(6));
+			vo.setFile(rs.getInt(7));
+			vo.setHit(rs.getInt(8));
+			vo.setUid(rs.getString(9));
+			vo.setRegip(rs.getString(10));
+			vo.setRdate(rs.getString(11));
+		}
+		
+		rs.close();
+		psmt.close();		
+		conn.close();
+		
+		return vo;
+	}
 	public void update() throws Exception {}
-	public void delete() throws Exception {}
+	public void delete(HttpServletRequest request) throws Exception {
+		request.setCharacterEncoding("UTF-8");
+		String seq = request.getParameter("seq");
+		
+		Connection conn = DBConfig.getConnection();
+		
+		PreparedStatement psmt = conn.prepareStatement(SQL.DELETE_BOARD);
+		psmt.setString(1, seq);
+		
+		psmt.executeUpdate();
+		psmt.close();
+		
+	}
 	public void insertComment() throws Exception {}
 	public void listComment() throws Exception {}
 }
